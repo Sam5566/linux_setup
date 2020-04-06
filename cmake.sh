4b50390ba99f387f3cc31cf6b25ec9b41c96d020
Sam5566 <samhuang87@gmail.com>
2020-04-06 14:12:55 +0800

    modified:   cmake.sh

--- cmake.sh
+++ cmake.sh
@@ -1,11 +1,6 @@
 #please run this file at $HOME/linux_setup
 
 DIR='setup_code/'
-if [ $? != 0 ]; then
-    echo please go to HOME/linux_setup
-    exit 0
-fi
-
 for FILE in "$DIR"/*.sh
 do
     echo Set up $FILE
