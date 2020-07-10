0752f850050bf90a56c56bf43a64bb79bb2a1861
Sam5566 <samhuang87@gmail.com>
2020-07-10 04:33:13 -0700

    modified:   setup_code/system_setup.sh 	modified:   setup_code/vim_setup.sh

--- setup_code/system_setup.sh
+++ setup_code/system_setup.sh
@@ -1,27 +1,37 @@
-# SSH
-echo    installing SSH...
+# Net-tool
+echo "	installing net-tool..."
 
-sudo apt-get install ssh
+sudo apt install net-tools
+
+# SSH
+echo "   installing SSH..."
+
+sudo apt-get -y install ssh
 netstat -a | grep ssh
 
 
 # build-essential
-echo    installing coding environment for C and C++...
+echo "   installing coding environment for C and C++..."
 
-sudo apt install build-essentail
+sudo apt install -y build-essential
 echo -----------------------------------
 echo check installation is successful or not
 gcc -v
 g++ -v
 make -v
-echo FINISH installation of build-essentiol
+echo FINISH installation of build-essential
 echo -----------------------------------
 
 
 # cmake
-echo installing CMake...
+echo "    installing CMake..."
 
-sudo apt install cmake
+sudo apt install -y cmake
 echo -----------------------------------
 cmake -v
+echo -----------------------------------
+
+echo FINISH
+echo -----------------------------------
+echo -----------------------------------
 echo -----------------------------------
