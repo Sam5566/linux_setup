72f458326c7b510c91917731ef333068d4693ebb
Sam5566 <samhuang87@gmail.com>
2020-07-11 11:47:30 +0800

    new file:   restore_settingfiles.sh

---
+++ restore_settingfiles.sh
@@ -0,0 +1,10 @@
+# This script copy those settingfiles in local computer and also shown in dir. settingfiles to dir. settingfiles/
+
+## zsh
+cp ~/.zshrc settingfiles/zshrc
+
+## vim
+cp -r ~/.vim settingfiles/vim
+cp ~.vimrc settingfiles/vimrc
+
+## root
