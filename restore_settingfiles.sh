7015bd22e4227346ad107cd68ceba5af58200be6
Sam5566 <samhuang87@gmail.com>
2020-07-12 13:54:55 +0800

    modified:   restore_settingfiles.sh 	new file:   settingfiles/start_madgraph.sh

--- restore_settingfiles.sh
+++ restore_settingfiles.sh
@@ -1,11 +1,14 @@
 # This script copy those settingfiles in local computer and also shown in dir. settingfiles to dir. settingfiles/
 
-## zsh
+# zsh
 cp ~/.zshrc settingfiles/zshrc
 
-## vim
+# vim
 cp -r ~/.vim settingfiles/vim
 cp ~/.vimrc settingfiles/vimrc
 
-## root
+# root
 cp
+
+# madgraph
+cp ~/HEP_tools/zshconfig/start_madgraph.sh
---
+++ settingfiles/start_madgraph.sh
@@ -0,0 +1,5 @@
+a=$PWD
+cd ~/MG5_aMC_v2_6_7
+./bin/mg5_aMC
+cd $a
+unset -v a
