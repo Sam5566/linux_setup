7ca2861b8d5f92202af44bccf6728154dc0d80b5
Sam5566 <samhuang87@gmail.com>
2020-07-12 01:32:06 -0700

    modified:   setup_code/anaconda_setup.sh

--- setup_code/anaconda_setup.sh
+++ setup_code/anaconda_setup.sh
@@ -1,17 +1,15 @@
 # Anaconda 3
-echo    downloading anaconda3
+echo    "    installing anaconda3..."
+cd ~/Downloads
 wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
+
 bash Anaconda3-2020.02-Linux-x86_64.sh
 source ~/.zshrc
 
+cd ~/linux_setup
 echo Check the installation of Anaconda3
 conda list
 
-# python3
-
-# numpy
-
-# matplotlib
 
 # scipy
 
