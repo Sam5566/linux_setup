0474817946bb4d87c67b957a767614f2e55b11e7
Sam5566 <samhuang87@gmail.com>
2020-07-13 14:34:07 +0800

    modified:   setup_code/1_terminal_setup.sh

--- setup_code/1_terminal_setup.sh
+++ setup_code/1_terminal_setup.sh
@@ -14,7 +14,7 @@ echo "    getting powerlevel9k..."
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 echo "    getting powerlevel10k..."
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 
 echo "    getting autojump..."
 git clone git://github.com/wting/autojump.git ~/ohmyzsh/custom/plugins/autojump
@@ -23,10 +23,10 @@ cd ~/ohmyzsh/custom/plugins/autojump
 cd ~/linux_setup
 
 echo "    getting zsh-syntax-highlighting..."
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/ohmyzsh/custom/plugins/zsh-syntax-highlighting
 
 echo "    getting zsh-autosuggestions..."
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone git://github.com/zsh-users/zsh-autosuggestions ~/ohmyzsh/custom/plugins/zsh-autosuggestions
 
 echo "    setting .zshrc..."
 cp settingfiles/zshrc ~/.zshrc
