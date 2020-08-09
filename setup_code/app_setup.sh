2a75d983df5f5bc328151a51f18964b65b552e35
Sam5566 <samhuang87@gmail.com>
2020-08-09 20:14:39 +0800

    modified:   setup_code/app_setup.sh 	modified:   setup_code/mathematica_setup.sh

--- setup_code/app_setup.sh
+++ setup_code/app_setup.sh
@@ -1,4 +1,8 @@
-cd ~/Download
-wget https://www.expandrive.com/download-expandrive/
-
+echo "installing Dropbox..."
+cd ~/Downloads
+echo "Please copy the location of the deb file from Dropbox installation page"
+firefox https://www.dropbox.com/zh_TW/install
+read dropbox_location
+wget "$dropbox_location"
+sudo dpkg -i dropbox*.deb
 cd ~/linux_setup
---
+++ setup_code/mathematica_setup.sh
@@ -0,0 +1,17 @@
+echo "Installing Mathematica..."
+cd ~/Downloads
+echo "Please download it from the opened page,"
+echo "As long as the shell script file is downloaded in ~/Downloads,"
+echo "Press ENTER"
+firefox "https://drive.google.com/u/1/uc?export=download&confirm=qApe&id=1Hhy8ejKFfPZ298CsFM4ZDWRqbOKra7LQ"
+read c
+#sudo bash Mathematica*.sh
+cd ~/linux_setup
+echo "---------------------------------------------------------"
+echo "NOTICE: Please break the Mathematica by following step"
+break_folder="https://drive.google.com/file/d/13ofeBlISKGzn_9OQ75pnOS5xx7Y6x8YG/view"
+echo "Please download folder from $break_folder in Windows,"
+echo "and execute the related bat flie."
+echo "You need to execute it when you are ask for the certification."
+echo "There is a manual in the folder, too."
+echo "---------------------------------------------------------"
